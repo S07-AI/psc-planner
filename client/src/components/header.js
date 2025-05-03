e93dc879353c94dd2ffa7d6742fefe9d162dc294
Shayan Safaei <shayansafaei1385@gmail.com>
2025-05-03 16:55:32 -0400

    Tailwind Update

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './header.css';
+import gryphonLogo from './gryphoncp.png'; // make sure it's inside the same folder
 
-function Header() {
-    return (
-        <header className = 'header'>
-            <h1 className = 'header-title'>Gryph Planner</h1>
-            <h2 className = 'header-subtitle'>Syllabi to Schedule.</h2>
-        </header>
-    )
-}
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={gryphonLogo} alt="Gryphon Logo" className="header-logo" />
+      <div className="header-text">
+        <h1 className="header-title">Course Calendar Converter</h1>
+        <p className="header-subtitle">Syllabi to Schedule</p>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
