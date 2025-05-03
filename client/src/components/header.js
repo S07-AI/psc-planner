8d080b0423af3166e78f074e4733932a27896442
caiden kowalchuk <caiden.kowalchuk@gmail.com>
2025-05-03 17:46:09 -0400

    adding the word to LOLOL

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="header-content">
         <img src={gryphonLogo} alt="Gryphon Logo" className="header-logo" />
         <div className="header-text">
-          <h1 className="header-title">Course Calendar Converter</h1>
+          <h1 className="header-title">Course to Calendar Converter</h1>
           <p className="header-subtitle">Syllabi to Schedule</p>
         </div>
       </div>
